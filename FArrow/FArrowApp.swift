c09431b70718bf110d007ba79bd6bebf3b551a7e
Daniel Rudrich <daniel.rudrich@atmoky.com>
2024-05-30 14:07:21 +0200

    Initial Commit

---
+++ FArrow/FArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FArrowApp.swift
+//  FArrow
+//
+//  Created by Daniel Rudrich on 30.05.24.
+//
+
+import SwiftUI
+
+@main
+struct FArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
